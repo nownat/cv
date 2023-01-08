04de223d1384562435c1a3c1516f9253346fb022
nownat <nowickan92@gmail.com>
2023-01-08 15:44:31 +0100

    Poprawki JS

--- javascript.js
+++ javascript.js
@@ -6,15 +6,7 @@ const onScroll = (event) => {
     const scrollPosition = 
     event.target.scrollingElement.scrollTop;
 
-    if (scrollPosition>150) {
-        banner.style.backgroundSize="150%";
-        h1.style.opacity = 0;
-        h1.style.translate="0-50px";
-        h1.style.scale ="0.9";
-        button.style.opacity = 0;
-        button.style.translate="0-40px";
-        button.style.scale ="0.8";
-    } else{
+    if (scrollPosition>100) {
         banner.style.backgroundSize="180%";
         h1.style.opacity = 1;
         h1.style.translate=0;
@@ -22,6 +14,14 @@ const onScroll = (event) => {
         button.style.opacity = 1;
         button.style.translate=0;
         button.style.scale =1;
+    } else{
+        banner.style.backgroundSize="90%";
+        h1.style.opacity = 1;
+        h1.style.translate="0-50px";
+        h1.style.scale ="0.9";
+        button.style.opacity = 1;
+        button.style.translate="0-40px";
+        button.style.scale ="0.8";
     }
 };
 
